46c9358454565b562fa7b179a9a31cc51b980506
Marconi <marconi@marconicivitavecchia.it>
2019-04-11 12:03:05 +0200

    creazione pagina dinamica

--- code/js/app.js
+++ code/js/app.js
@@ -7,6 +7,24 @@ var app = {
 	},
 	showCards(dataJson) {
 		console.log(dataJson);
+		dataJson.cardList.forEach(element => {
+			let card = `
+			
+			<div class="col-lg-3 col-md-6 mb-4 net-all">
+			  <div class="card h-100">
+				<img class="card-img-top" src="${element.imgUrl}" alt="Moodle">
+				<div class="card-body">
+				  <h4 class="card-title">${element.title}</h4>
+				  <p class="card-text">${element.desc}</p>
+				</div>
+				<div class="card-footer">
+				  <a href="${element.buttonUrl}" class="btn btn-primary">Accedi</a>
+				</div>
+			  </div>
+			</div>`;
+			$("#card-container").append(card)
+			
+		});
 		
 	},
 	onError: function(e) {
